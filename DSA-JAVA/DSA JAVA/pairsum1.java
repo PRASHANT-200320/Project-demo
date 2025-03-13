43f3a22977f9bc3995eb3f528a07c015914713fb
PRASHANT-200320 <prashant.shringi20@gmail.com>
2025-03-13 17:06:44 +0530

    Add files via upload

---
+++ DSA-JAVA/DSA JAVA/pairsum1.java
@@ -0,0 +1,126 @@
+import java.util.*;
+public class pairsum1 {
+
+
+   //TC=o(n2)
+
+    // public static boolean pairsum1(ArrayList<Integer>list , int target){
+
+    //     for(int i=0;i<list.size();i++){
+            
+    //     for(int j=i+1;j<list.size();j++){
+
+    //         if(list.get(i)+list.get(j)==target){
+    //             return true;
+    //         }
+            
+    //     }
+    //     }
+
+    //     return false;
+    // }
+         
+
+
+
+
+    public static boolean pairsum1(ArrayList<Integer>list , int target){
+
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp!=rp){
+
+            if(list.get(lp)+list.get(rp) == target){
+                return true;
+            }
+
+            
+            if(list.get(lp)+list.get(rp) < target){
+               lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return false;
+
+
+    }
+
+    public static void main(String args[]) {
+        
+
+       ArrayList<Integer> list =new ArrayList<>();
+       list.add(2);
+      list.add(3);
+       list.add(4);
+        list.add(5);
+         list.add(6);
+          list.add(7);
+           list.add(8);
+            list.add(9);
+
+       System.out.print(pairsum1(list,8));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DSA-JAVA/DSA JAVA/trappwater.java
@@ -0,0 +1,130 @@
+import java.util.*;
+public class trappwater {
+
+    // public static int trapp(int height[]){
+    //      int n=height.length;
+    //     int leftmax[] = new int[n];
+    //  leftmax[0] = height[0];
+    //     for(int i=1;i<n;i++){
+    //    leftmax[i] = Math.max(leftmax[i-1],height[i]);
+    //     }
+    //     int rightmax[] = new int[n];
+    //     rightmax[n-1] = height[n-1];
+    //     for(int i=n-2;i>=0;i--){
+    //    rightmax[i] = Math.max(rightmax[i+1],height[i]);
+    //     }
+     
+    //   int trappwater=0;
+    //   for(int i=0; i<n;i++){
+    //    int waterlevel =Math.min(leftmax[i],rightmax[i]);
+    //     trappwater +=waterlevel-height[i];
+    //   }
+    //   return trappwater;
+
+    // }
+
+
+
+question 5 not solve
+
+
+
+
+    public static void main(String args[]) {
+        Scanner SC = new Scanner(System.in);
+
+       int height[] = {4,2,0,3,2,5};
+      
+System.out.print(trapp(height));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    //   for(int i=1;i<=n;i++) {
+
+    //     for(int j=1;j<=i;j++){
+    //           System.out.print("*");
+    //     }
+    //     for(int j=1;j<=2*(n-i);j++) {
+    //           System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //            System.out.print("*");
+    //     }
+    //        System.out.println();
+    //   }
+    //      for(int i=n;i>=1;i--){
+    //          for(int j=1;j<=i;j++)
+    //           System.out.print("*");
+    //      }
+    //      for(int j=1;j<=2*(n-i);j++) {
+    //           System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //            System.out.print("*");
+    //     }
+    //      System.out.println();
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
